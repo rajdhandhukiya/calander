8ea4afd416d1120ecf8c8ce50458f2ce0add7c95
keval gondaliya <keval.gondaliya.km@gmail.com>
2022-03-23 15:50:02 +0530

    form adding

--- src/Form.js
+++ src/Form.js
@@ -5,11 +5,14 @@ import edit from "../src/Edit.svg";
 import removal2 from "../src/removal2.svg";
 
 import "react-calendar/dist/Calendar.css";
+import "../src/form.css";
 function FormComponent() {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
+  const [image, setImage] = useState();
   const [password, setPassword] = useState();
   const [bdate, setBdate] = useState();
+  const [saveImage, setSaveImage] = useState({});
   const [data, setData] = useState([]);
   const [dataIndex, setDataIndex] = useState();
   const [isButton, setIsButton] = useState(true);
@@ -23,8 +26,9 @@ function FormComponent() {
 
     const formData = new FormData(e.target),
       formDataObj = Object.fromEntries(formData.entries());
+    formDataObj.image = saveImage;
+    console.log("saveimage", saveImage);
     console.log(formDataObj);
-
     if (isButton) setData([...data, formDataObj]);
     else {
       let arr = data;
@@ -43,6 +47,7 @@ function FormComponent() {
   }
 
   const clear = () => {
+    setImage("")
     setName("");
     setEmail("");
     setPassword("");
@@ -63,65 +68,87 @@ function FormComponent() {
     setIsButton(false);
     setDataIndex(index);
   };
- 
+
+  const handleChange = (event) => {
+    setSaveImage(event.target.files[0]);
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
+  console.log("data", data);
   return (
     <div>
-     
       <div className="Form_Main">
         <form onSubmit={handleSubmit}>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="mb-3 image_div">
+            <input
+              type="file"
+              accept="image/jpeg,image/png"
+              className="form-control image_Upload"
+              name="image"
+              onChange={(event) => {
+                console.log("file", event.target.files[0]);
+                handleChange(event);
+              }}
+            />
+            <img className="image_set" src={image} />
+          </div>
+
+          <div className="mb-3">
+            <label for="exampleInputName1" className="form-label">
               Name
             </label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               name="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
 
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
               Email
             </label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               name="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               name="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label for="exampleInputDateOfBirth1" className="form-label">
               Date Of Birth
             </label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               name="dob"
               value={bdate}
               onChange={(e) => setBdate(e.target.value)}
             />
           </div>
 
-          <button type="submit" class="btn btn-primary" disabled={inputIsEmpty}>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={inputIsEmpty}
+          >
             {isButton ? "submit" : "update"}
           </button>
         </form>
@@ -129,6 +156,7 @@ function FormComponent() {
       <Table striped bordered hover>
         <thead>
           <tr>
+            <th>image</th>
             <th>name</th>
             <th>email</th>
             <th>password</th>
@@ -140,6 +168,7 @@ function FormComponent() {
           {data?.map((item, index) => {
             return (
               <tr key={item.id}>
+                <td><img className="image_set2" src={URL.createObjectURL(item.image)} alt="demo" /></td>
                 <td>{item.name}</td>
                 <td>{item.email}</td>
                 <td>{item.password}</td>
